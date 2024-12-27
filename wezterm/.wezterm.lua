039b754ac0845d2e8e4c7e887d14d580ae24e797
corwinm <corwinm@users.noreply.github.com>
2024-12-27 15:28:00 -0800

    Wezterm updates

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,9 +7,16 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
--- config.color_scheme = "Catppuccin Macchiato"
+config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.font_size = 16.0
+-- config.color_scheme = "Catppuccin Macchiato"
+config.colors = {
+	cursor_bg = "white",
+	cursor_border = "white",
+}
+
+config.max_fps = 120
 
 -- and finally, return the configuration to wezterm
 return config
