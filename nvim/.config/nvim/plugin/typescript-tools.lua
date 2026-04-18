d1eb14bcae7bfd2553f04f0b991b4d56083ae6fd
Corwin Marsh <7642548+corwinm@users.noreply.github.com>
2026-04-18 00:32:11 -0700

    lazy load typescript-tools

--- nvim/.config/nvim/plugin/typescript-tools.lua
+++ nvim/.config/nvim/plugin/typescript-tools.lua
@@ -1,7 +1,27 @@
-vim.pack.add {
-  'https://github.com/pmizio/typescript-tools.nvim',
+local ts_spec = {
+  src = 'https://github.com/pmizio/typescript-tools.nvim',
+  name = 'typescript-tools.nvim',
 }
-require('typescript-tools').setup {
-  single_file_support = false,
-  root_dir = require('lspconfig').util.root_pattern 'package.json',
-}
+
+vim.pack.add({ ts_spec }, { load = false })
+
+local did_setup = false
+
+local function setup_typescript_tools()
+  if did_setup then return end
+  did_setup = true
+
+  require('typescript-tools').setup {
+    single_file_support = false,
+    root_dir = require('lspconfig').util.root_pattern 'package.json',
+  }
+end
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+  once = true,
+  callback = function()
+    vim.cmd.packadd 'typescript-tools.nvim'
+    setup_typescript_tools()
+  end,
+})
