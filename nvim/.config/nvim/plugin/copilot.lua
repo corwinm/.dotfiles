1c3ae97210fea4bf1c4277bee8058c9a73cdd023
Corwin Marsh <corwinm@users.noreply.github.com>
2026-05-12 14:04:11 -0700

    Update copilot config

--- nvim/.config/nvim/plugin/copilot.lua
+++ nvim/.config/nvim/plugin/copilot.lua
@@ -1,28 +1,13 @@
-vim.api.nvim_create_autocmd('InsertEnter', {
-  once = true,
-  callback = function()
-    -- Load the Copilot plugin lazily when entering insert mode for the first time
-    vim.pack.add {
-      {
-        src = 'https://github.com/zbirenbaum/copilot.lua',
-        cmd = 'Copilot',
-      },
-    }
+vim.pack.add {
+  'https://github.com/zbirenbaum/copilot.lua',
+}
 
-    require('copilot').setup {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-      },
-    }
-
-    vim.keymap.set('i', '<Tab>', function()
-      if require('copilot.suggestion').is_visible() then
-        require('copilot.suggestion').accept()
-      else
-        -- Fallback to default Tab behavior (e.g., indentation)
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
-      end
-    end, { silent = true })
-  end,
-})
+require('copilot').setup {
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    keymap = {
+      accept = '<Tab>',
+    },
+  },
+}
